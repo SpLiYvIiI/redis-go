47461018070d64832149949044c37447f5fe67d2
KutuGaming <xellaspc@DESKTOP-5RD3MIB.localdomain>
2024-11-02 01:21:48 +0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -11,33 +12,34 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	buf := make([]byte, 128)
-	_, err = c.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading message from connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	_, err = c.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing message to connection: ", err.Error())
-		os.Exit(1)
-	}
-
 	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleMessagesForConnection(conn)
+	}
+}
+
+func handleMessagesForConnection(connection net.Conn) {
+	for {
+		message := make([]byte, 256)
+		_, err := connection.Read(message)
+		if err != nil {
+			fmt.Printf("Error reading message from connection %s\n", err.Error())
+		}
+		fmt.Println(message)
+
+		_, err = connection.Write([]byte("+PONG\r\n"))
+
+		if err != nil {
+			fmt.Printf("Error writing message to connection %s\n", err.Error())
+		}
+	}
+
 }
